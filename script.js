0450029830f7960407c7c39a0c3947772bfe525b
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-20 00:31:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+let newTodoInput = document.getElementById("newTodoInput");//input
 
+let todoList = document.getElementById("todoList");//ol
+
+
+let btn = document.getElementById("addTodoBtn");//btn
+
+// btn.addEventListener("click",f());
+
+function f(){
+	let newele = document.createElement("li");
+	newele.textContent  = newTodoInput.value;
+	todoList.appendChild(newele)
+}
